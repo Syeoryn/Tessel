87c867bdc8c9dad73693f8aac22f8fe1bb4de878
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-19 11:46:56 -0700

    Adds volume control to blinkyaccel

--- blinkyaccel.js
+++ blinkyaccel.js
@@ -5,6 +5,7 @@ var led1 = tessel.led[0].output(0);
 var led2 = tessel.led[1].output(0);
 
 var timer;
+var volume = 0;
 
 accel.on('ready', function(){
   accel.on('data', function(xyz){
@@ -14,6 +15,8 @@ accel.on('ready', function(){
     console.log('x:',x.toFixed(2),
       'y:', y.toFixed(2),
       'z:', z.toFixed(2));
+    volume = (x + 1) / 2;
+    console.log(volume);
     if(x > 0){
       led1.output(1);
     } else {
