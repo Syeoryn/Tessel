363c766aa0e9c42fc89f87a9c40db112806f704c
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-19 13:57:07 -0700

    Tests NRF

--- NRF/nRF.js
+++ NRF/nRF.js
@@ -6,7 +6,7 @@
 var tessel = require('tessel'),
   NRF24 = require('rf-nrf24'),
   pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2],
-  role = 'ping'; // swap this to pong if you want to wait for receive
+  role = 'pong'; // swap this to pong if you want to wait for receive
 
 var nrf = NRF24.channel(0x4c) // set the RF channel to 76. Frequency = 2400 + RF_CH [MHz] = 2476MHz
   .transmitPower('PA_MAX') // set the transmit power to max
@@ -16,7 +16,6 @@ var nrf = NRF24.channel(0x4c) // set the RF channel to 76. Frequency = 2400 + RF
   .use(tessel.port['A']);
 
 nrf._debug = false;
-
 nrf.on('ready', function () {
   setTimeout(function(){
     nrf.printDetails();
@@ -42,10 +41,10 @@ nrf.on('ready', function () {
     });
   } else {
     console.log("PONG back");
-    var rx = nrf.openPipe('rx', pipes[0], {size: 4});  
+    var rx = nrf.openPipe('rx', pipes[0], {size: 64});  
       tx = nrf.openPipe('tx', pipes[1], {autoAck: false}); 
     rx.on('data', function (d) {
-      console.log("Got data, will respond", d);
+      console.log("Got data, will respond", d.toString());
       tx.write(d);
     });
     tx.on('error', function (e) {
