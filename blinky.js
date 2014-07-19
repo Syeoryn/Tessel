0c99310d715de9d94e94b4f3562434ffc077569a
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-19 11:11:59 -0700

    Adds leds to blinky

--- blinky.js
+++ blinky.js
@@ -1,11 +1,20 @@
 var tessel = require('tessel');
 
-var led1 = tessel.led[0].output(1);
-var led2 = tessel.led[1].output(0);
+var led1 = tessel.led[0].output(0);
+var led2 = tessel.led[1].output(1);
+var led3 = tessel.led[2].output(0);
+var led4 = tessel.led[3].output(1);
 
 setInterval(function() {
   console.log("Blink blink");
 
   led1.toggle();
   led2.toggle();
+  led3.toggle();
+  led4.toggle();
 }, 100);
+
+
+// tessel update
+// tessel run blinky.js
+// now, get the lights to blink in sync...
